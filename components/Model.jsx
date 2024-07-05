cc41dad48ddac9a81e265e26fb13e647898a69cf
Divine Charles <officialkb17@gmail.com>
2024-07-05 09:26:03 +0100

    Remove leva control in prod

--- components/Model.jsx
+++ components/Model.jsx
@@ -1,8 +1,8 @@
 import React, { useRef } from 'react';
-import { useControls } from 'leva';
-import { fragment, vertex } from '@/shaders/Shader';
 import { useFrame } from '@react-three/fiber';
 import { useTexture, useAspect } from '@react-three/drei';
+import { fragment, vertex } from '@/shaders/Shader';
+import { useControls } from 'leva';
 
 const Model = () => {
   const image = useRef();
@@ -12,10 +12,18 @@ const Model = () => {
   const { width, height } = texture.image;
   const scale = useAspect(width, height, 0.3);
 
-  const { amplitude, waveLength } = useControls({
-    amplitude: { value: 0.25, min: 0, max: 2, step: 0.1 },
-    waveLength: { value: 5, min: 0, max: 20, step: 0.5 },
-  });
+  const isDevelopment = process.env.NODE_ENV === 'development';
+  const controls = useControls(
+    isDevelopment
+      ? {
+          amplitude: { value: 0.25, min: 0, max: 2, step: 0.1 },
+          waveLength: { value: 5, min: 0, max: 20, step: 0.5 },
+        }
+      : {},
+  );
+
+  const amplitude = isDevelopment ? controls.amplitude : 0.25;
+  const waveLength = isDevelopment ? controls.waveLength : 5;
 
   const uniforms = useRef({
     uTime: { value: 0 },
